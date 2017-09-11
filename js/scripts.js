41792b548e8104cefaeff759399551a80a161eb2
sim1 <rafal.leszczynski@simplica.pl>
2017-09-11 13:47:42 +0200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+var name = prompt('Enter your name');//definition of "name"
+alert('Hello, ' + name);//display on the screen
+console.log('Hello, ' + name);//display into the console
+/*
+Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus e
+*/
